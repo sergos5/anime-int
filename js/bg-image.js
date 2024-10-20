caa97ddc68331ea739bb22de25f7c7a9c50de9c2
Sergey Opekunov <sergos5@yandex.ru>
2024-10-20 22:07:07 +0300

    Добавил вывод изображений, модальное окно, preloder

--- js/bg-image.js
+++ js/bg-image.js
@@ -2,8 +2,7 @@ const elements = document.querySelectorAll(".set-bg")
 
 for(let i = 0; i<elements.length; i++) {
     const src = elements[i].dataset.setbg
-    elements[i].style.backgroundImage = `url(${src})`   
-    console.log(src)
+    elements[i].style.backgroundImage = `url(${src})`      
 }
 
 
