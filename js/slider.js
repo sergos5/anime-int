5315e307429c612939c45e400e37e1a007887138
Sergey Opekunov <sergos5@yandex.ru>
2024-10-21 15:51:47 +0300

    добавил swiper-slider

---
+++ js/slider.js
@@ -0,0 +1,25 @@
+const slider = function() {
+    
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        //direction: 'vertical',
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        effect: "fade",
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },    
+    });          
+}
+
+slider()
