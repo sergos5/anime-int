c9545a26bacce2e803c7254346433350bbe52fb0
Sergey Opekunov <sergos5@yandex.ru>
2024-10-23 10:34:42 +0300

    фильтр по топ 5 просмотров, извенил пути к bg к ним в db.json

--- js/mainData.js
+++ js/mainData.js
@@ -1,12 +1,38 @@
 const mainData = ()=> {
+
+    const renderTopViews = (arrTopViews) => {
+        const wrapper = document.querySelector('.filter__gallery')
+        wrapper.innerHTML=''
+        arrTopViews.forEach(item => {   
+            console.log(item);
+            
+            wrapper.insertAdjacentHTML('beforeend', `
+                <div class="product__sidebar__view__item set-bg mix"
+                    data-setbg="${item.image}">
+                    <div class="ep"> ${item.rating} / 10</div>
+                    <div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
+                    <h5><a href="/anime-details.html">${item.title}</a></h5>
+                </div>`     
+            );    
+        })
+       
+        wrapper.querySelectorAll(".set-bg").forEach((item)=> {
+            item.style.backgroundImage = `url(${item.dataset.setbg})`
+        }) 
+
+    }
+
     fetch('./db.json')
         .then((response)=> {
             return response.json()
         })
         .then((data)=> {
-            console.log(data.anime);            
+            renderTopViews(data.anime.sort((a,b) => b.views - a.views).slice(0,5));  
+            
+            
         }) 
     
 }
 
 mainData()
+
