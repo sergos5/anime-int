30ed8d72fbab580486dd500e009c18950181c69d
Sergey Opekunov <sergos5@yandex.ru>
2024-10-23 13:42:45 +0300

    добавил вывод карточек по жанрам, получая их из db.json

--- js/mainData.js
+++ js/mainData.js
@@ -1,9 +1,64 @@
 const mainData = ()=> {
 
     const renderAnimeList = (arrAnime, ganres) => {
-        console.log(ganres)
-        console.log(arrAnime);
-        
+        const wrapper = document.querySelector('.product .col-lg-8')
+        wrapper.innerHTML = ''
+
+        ganres.forEach(ganre => {
+            const productBlock = document.createElement('div')
+            const listBlock = document.createElement('div')      
+            const list = arrAnime.filter(item => item.ganre === ganre)
+               
+            listBlock.classList.add('row')
+            
+            listBlock.classList.add('mb-5')
+            productBlock.insertAdjacentHTML('beforeend', `
+                <div class="row">
+                    <div class="col-lg-8 col-md-8 col-sm-8">
+                        <div class="section-title">
+                            <h4>${ganre}</h4>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4">
+                        <div class="btn__all">
+                            <a href="/categories.html" class="primary-btn">View All <span class="arrow_right"></span></a>
+                        </div>
+                    </div>
+                </div>
+            `)    
+            
+            list.forEach(item => {                   
+                const tagsBlock = document.createElement('ul')
+
+                item.tags.forEach(tag => {
+                    tagsBlock.insertAdjacentHTML('beforeend', `
+                        <li>${tag}</li>     
+                    `)
+                })
+                
+                listBlock.insertAdjacentHTML('afterbegin', `
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="product__item">
+                            <div class="product__item__pic set-bg" data-setbg="${item.image}">
+                                <div class="ep">${item.rating} / 10</div>
+                                <div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
+                            </div>
+                            <div class="product__item__text">
+                                ${tagsBlock.outerHTML}
+                                <h5><a href="/anime-details.html">${item.title}</a></h5>
+                            </div>
+                        </div>
+                    </div>
+                `)
+            })
+
+            wrapper.append(productBlock)
+            wrapper.append(listBlock)
+
+            wrapper.querySelectorAll(".set-bg").forEach((item)=> {
+                item.style.backgroundImage = `url(${item.dataset.setbg})`
+            })
+        })        
     }
 
     const renderTopViews = (arrTopViews) => {
